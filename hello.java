6612f47cf5d4955f89c908f61758956394689bd5
indirwanhsb14 <indirwan.ihsan.hasibuan@gmail.com>
2022-06-14 17:28:21 +0700

    Create hello.java
    JAVA SYNTAX'S

---
+++ hello.java
@@ -0,0 +1,15 @@
+// package com.Hacktive8.program; //<- deklarasi package
+
+// import java.io.File; //<- Import Library
+
+public class hello { // <- Bagian class
+    public static void main(String[] args) { // <- Method Main
+        System.out.println("Halo"); // <- Statement
+
+        var x = 3; // <- Expressions
+        var y = 9;
+        var z = x + y;
+        System.out.println("Ini Total Bill Anda : " + z);
+        System.out.println("Terimakasih");
+    }
+}
