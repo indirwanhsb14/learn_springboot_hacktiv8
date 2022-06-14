6d6f84517cd369e8bd81c99289efe1d5aa30c3bb
indirwanhsb14 <indirwan.ihsan.hasibuan@gmail.com>
2022-06-14 19:51:03 +0700

    PrintVSPrintln
    Learn different syntax print and println

---
+++ PrintVSPrintln.java
@@ -0,0 +1,7 @@
+public class PrintVSPrintln {
+    public static void main(String[] args) {
+        System.out.print("Ini adalah teks print()");
+        System.out.println("Sedangkan ini adalah teks println()");
+        System.out.print("Ini adalah teks print() lagi ");
+    }
+}
